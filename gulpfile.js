0562969565e756c8c8bd7ae90f64fdde147ad430
Mike Collins <intervalia@gmail.com>
2014-12-04 09:03:18 -0700

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 var Mocha = require('mocha');
+var components = require('./src/index.js');
+var oldTemplatePlugin = require("./plugins/oldTemplates");
+
+var assemblyPath = "/Users/collinsmg/projects/tree/frontier-tree/assets/js/modules/**/assembly.json";
+    //assemblyPath = "testdata/**/assembly.json";
 
 gulp.task('lint', function () {
   return gulp.src([
@@ -12,6 +19,26 @@ gulp.task('lint', function () {
   .pipe(jshint({"esnext": true}))
   .pipe(jshint.reporter('default', { verbose: true }))
   .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('build', function() {
+  components.addPlugin( components.pluginType.INLINE, oldTemplatePlugin );
+
+  return(
+    gulp.src(assemblyPath)
+        .pipe(components.assemble({
+          "defaultLocale": 'en',
+          "minTemplateWS": true,
+          "supportTransKeys": false,
+          "useExternalLib": true
+        }))
+        .pipe(gulp.dest('./prod'))
+        .pipe(uglify())
+        .pipe(rename(function (path) {
+          path.basename += "-min";
+          }))
+        .pipe(gulp.dest('./prod'))
+  );
 });
 
 gulp.task('test', ['lint'], function (done) {
